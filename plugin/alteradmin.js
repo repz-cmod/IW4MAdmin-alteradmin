503422d59bcb715e2d398720870452d18b342b47
Sepehr Ghorbanpoor <sepehrgh01@gmail.com>
2021-04-09 15:07:06 +0430

    feat: command suppot
    ts, spec, tp, blc, spawn

--- plugin/alteradmin.js
+++ plugin/alteradmin.js
@@ -1,31 +1,106 @@
-let commands = [
-    {
-        name: "teamswitch",
-        description: "Switches a player team",
-        alias: "pps",
-        permissions: "Administrator",
+var permission_error = "Your rank is lower than "
 
-        execute: (gameEvent) => {
-            var server = gameEvent.Owner;
-            var cid = gameEvent.Target.ClientNumber;
-            if (gameEvent.Origin.Level > gameEvent.Target.Level){
-                server.SetDvarAsync("g_switchteam", ''+ cid)
-                // server.RconParser.ExecuteCommandAsync(server.RemoteConnection, 'set sv_b3Execute !quickmaths ' + cid + ' ' + message).Result;
-            }
-        }
+let commands = [{
+    name: "teamswitch",
+    description: "Switches a player's team",
+    alias: "ts",
+    permission: "Administrator",
+    targetRequired: true,
+    arguments: [{
+        name: "Target Player",
+        required: true
+    }],
+    execute: (gameEvent) => {
+        var server = gameEvent.Owner;
+        var cid = gameEvent.Target.ClientNumber;
+        if (gameEvent.Origin.Level > gameEvent.Target.Level)
+            server.RconParser.SetDvarAsync(server.RemoteConnection, "g_switchteam", ''+ cid.toString())
+        else
+            gameEvent.Origin.Tell(permission_error + gameEvent.Target.Name);
     }
-]
+}, {
+    name: "spectate",
+    description: "Switches a player to spectator mode",
+    alias: "spec",
+    permission: "Administrator",
+    targetRequired: true,
+    arguments: [{
+        name: "Target Player",
+        required: true
+    }],
+    execute: (gameEvent) => {
+        var server = gameEvent.Owner;
+        var cid = gameEvent.Target.ClientNumber;
+        if (gameEvent.Origin.Level > gameEvent.Target.Level)
+            server.RconParser.SetDvarAsync(server.RemoteConnection, "g_switchspec", ''+ cid.toString())
+        else
+            gameEvent.Origin.Tell(permission_error + gameEvent.Target.Name);
+    }
+}, {
+    name: "teleport",
+    description: "Teleports a player to another player",
+    alias: "tp",
+    permission: "SeniorAdmin",
+    targetRequired: true,
+    arguments: [{
+        name: "Target Player",
+        required: true
+    }],
+    execute: (gameEvent) => {
+        var server = gameEvent.Owner;
+        var cid = gameEvent.Target.ClientNumber;
+        server.RconParser.SetDvarAsync(server.RemoteConnection, "g_teleport", ''+ cid.toString())
+    }
+}, {
+    name: "balance",
+    description: "Balances teams",
+    alias: "blc",
+    permission: "Administrator",
+    targetRequired: true,
+    arguments: [{
+        name: "Target Player",
+        required: true
+    }],
+    execute: (gameEvent) => {
+        var server = gameEvent.Owner;
+        var cid = gameEvent.Target.ClientNumber;
+        server.RconParser.SetDvarAsync(server.RemoteConnection, "g_balance", ''+ cid.toString())
+    }
+}, {
+    name: "spawn",
+    description: "Spawns a player",
+    alias: "spw",
+    permission: "Administrator",
+    targetRequired: true,
+    arguments: [{
+        name: "Target Player",
+        required: true
+    }],
+    execute: (gameEvent) => {
+        var server = gameEvent.Owner;
+        var cid = gameEvent.Target.ClientNumber;
+        server.RconParser.SetDvarAsync(server.RemoteConnection, "b3_spawn", ''+ cid.toString())
+    }
+}
+
+];
 
 let plugin = {
     author: 'sepehr-gh',
-    version: 1.1,
-    name: 'Alteradmin',
+    version: 1.0,
+    name: 'RepZ Alteradmin',
 
-    onEventAsync: function(gameEvent, server) {},
+    onEventAsync: function (gameEvent, server) {
+    },
 
-    onLoadAsync: function(manager) {},
+    onLoadAsync: function (manager) {
+        this.logger = manager.GetLogger(0);
+        this.logger.WriteDebug("RepZ Alteradmin loaded");
+    },
 
-    onUnloadAsync: function() {},
+    onUnloadAsync: function () {
+    },
 
-    onTickAsync: function(server) {}
+    onTickAsync: function (server) {
+    }
 };
